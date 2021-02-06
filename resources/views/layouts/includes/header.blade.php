7ff80d6901c901eb5a9e7127b8b603c87b7fd668
coffeequotes <sky8052785942ocean@gmail.com>
2021-02-06 12:03:22 +0530

    updated header

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -1,19 +1,43 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="{{setting('site.description')}}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
+    <meta name="description" content="{{setting('site.description')}}">
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{Request::url()}}">
+    <meta property="og:title" content="{{ setting('site.title') }}">
+    <meta property="og:image" content="{{asset("app-assets/img/core-img/all-in-one.png")}}">
+    <meta property="og:description" content="{{setting('site.description')}}">
+    <meta property="og:site_name" content="Tneos Eduloutions Ltd.">
+    <meta property="og:locale" content="en_US">
+    <!-- Next tags are optional but recommended -->
+    <meta property="og:image:width" content="833">
+    <meta property="og:image:height" content="1185">
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content"{{Request::url()}}">
+    <meta name="twitter:creator" content="@TneosL">
+    <meta name="twitter:url" content="{{Request::url()}}">
+    <meta name="twitter:title" content="{{ setting('site.title') }}">
+    <meta name="twitter:description" content="{{setting('site.description')}}">
+    <meta name="twitter:image" content="{{asset("app-assets/img/core-img/all-in-one.png")}}">
+
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Title -->
     {{--    <title>{{ config('app.name', 'Laravel') }} </title>--}}
     <title>@yield('page_title', setting('site.title') . " - " . setting('site.description'))</title>
     <!-- Favicon -->
-    <link rel="icon" href="{{asset("app-assets/img/core-img/favicon.ico")}}">
+
+    <link rel="icon" type="image/x-icon" href="{{asset("app-assets/img/core-img/favicon.ico")}}">
+    <link rel="canonical" href="{{Request::url()}}">
     <!-- Core Stylesheet -->
     <link href="http://fonts.googleapis.com/css?family=Corben:bold" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Nobile" rel="stylesheet" type="text/css">
@@ -37,10 +61,8 @@
 <!-- ##### Header Area Start ##### -->
 
 <!--<header class="header-area" style=" background-image: url({{asset("app-assets/img/core-img/small-banner.png")}});   background-size: cover;  background-color: #333333; background-blend-mode: overlay; "> -->
-<header class="header-area" style="background-color: #000000; ">
-
+<header class="header-area" style="background-color: #000000;">
     <!-- Top Header Area -->
-{{--    #3f206c;--}}
 {{--    <div class="top-header">--}}
 {{--        <div class="container-fluid h-100">--}}
 {{--            <div class="row h-100">--}}
@@ -133,13 +155,13 @@
 {{--       --}}
 {{--    </div>--}}
 @include('layouts.includes.partials.navbar')
-    <!-- Calling Info -->
-{{--    <div class="calling-info">--}}
-{{--        <div class="call-center">--}}
-{{--            <a href="tel:+91{{setting('site.customer_care')}}"><i class="icon-telephone-2"></i>--}}
-{{--                <span>(+91) {{setting('site.customer_care')}}</span></a>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+<!-- Calling Info -->
+    {{--    <div class="calling-info">--}}
+    {{--        <div class="call-center">--}}
+    {{--            <a href="tel:+91{{setting('site.customer_care')}}"><i class="icon-telephone-2"></i>--}}
+    {{--                <span>(+91) {{setting('site.customer_care')}}</span></a>--}}
+    {{--        </div>--}}
+    {{--    </div>--}}
     </nav>
     </div>
     </div>
